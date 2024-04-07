19093b690162cfa62109d22dd4669ca070dbd864
SRoberts <25109326+SR6@users.noreply.github.com>
2024-04-06 21:56:19 -0500

    Added stuff for recyclerview and adapter for transition from categoryFragment to ItemsFragment. Something about safeargs is breaking the gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id ("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
+    //id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
+    //id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
 }
