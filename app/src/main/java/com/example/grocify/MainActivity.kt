6465eea6e3d4291c5d6a69bb345d1e6d0410fc54
SRoberts <25109326+SR6@users.noreply.github.com>
2024-04-13 17:11:40 -0500

    Added hiding keyboard after search is received

--- app/src/main/java/com/example/grocify/MainActivity.kt
+++ app/src/main/java/com/example/grocify/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.grocify
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -27,6 +28,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var authUser : AuthUser
 
+    // An Android nightmare
+    // https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+    // https://stackoverflow.com/questions/7789514/how-to-get-activitys-windowtoken-without-view
+    fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -63,6 +71,7 @@ class MainActivity : AppCompatActivity() {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (!query.isNullOrEmpty()) {
                     Log.d("search", "MainActivity SearchButton Clicked $query")
+                    hideKeyboard()
                     lifecycleScope.launch {
                         //commented out to reduce requests to Kroger
                         viewModel.getProducts(query)
